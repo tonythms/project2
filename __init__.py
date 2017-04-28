a9fb7d447fe71820533f056465c94798a4f4d453
tonythms <tony.thomas@da-cons.de>
2017-04-28 13:49:08 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,7 @@
+
+#!/usr/bin/env python
+"""
+Project1 is Steve's example project for the blog.
+It contains sample code here and there.
+"""
+
---
+++ sum.py
@@ -0,0 +1,8 @@
+# This program adds two numbers
+"""its fun """
+N1 = 1
+N2 = 6
+# Add two numbers
+S = N1+N2
+# Display the sum
+print 'The sum of {0} and {1} is {2}' .format(N1, N2, S)
