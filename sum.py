fa09fe3711112004d6e66283d307342f33d563b4
tonythms <tony.thomas@da-cons.de>
2017-04-28 15:20:58 +0200

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,8 +1,8 @@
 # This program adds two numbers
 """its fun """
 N1 = 8
-N2 = 14
+N2 = 147
 # Add two numbers
-S = N1+N3
+S = N1+N2
 # Display the sum
 print 'The sum of {0} and {1} is {2}' .format(N1, N2, S)
