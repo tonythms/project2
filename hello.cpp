1d4aba1784e16a2e9b8866a39cbfa86cca4c6187
tonythms <tony.thomas@da-cons.de>
2017-05-03 10:36:25 +0200

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+/* C++ Programming Examples */
+		
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	clrscr();  // clear the screen
+	cout<<"Hello Compiler, I am C++";
+	getch();  // holds output screen until user press a key
+}
