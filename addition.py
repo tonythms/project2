fb63928a3b311b54753bfeb658ee3e8cce460ca7
tonythms <tony.thomas@da-cons.de>
2017-05-29 17:17:04 +0200

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,10 +1,10 @@
 """prgm"""
 def add(att1, att2):
     """prgm"""
-    if att1.isdigit() and att2.isdigit():
-        return att1 + att2
-    else:
+    if ((att1 + att2) % 10) - att2 == 0:
         return False
+    else:
+        return att1 + att2
 
 # take input from the user
 print "Addition of two numbers"
