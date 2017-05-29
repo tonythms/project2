873df0ef3e16fa6f2bfff733c4b472b180359f70
tonythms <tony.thomas@da-cons.de>
2017-05-29 13:04:50 +0200

    Update addition.py

--- addition.py
+++ addition.py
@@ -9,7 +9,7 @@ def add(x, y):
 # take input from the user
 print "Addition of two numbers"
 
-int(n1) = input("Enter first number: ")
-int(n2) = input("Enter second number: ")
+n1 = input("Enter first number: ")
+n2 = input("Enter second number: ")
 
 print n1, "+", n2, "=", add(n1, n2)
