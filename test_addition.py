194f45c348090d449e9df40ff7a89dbc4b3668f8
tonythms <tony.thomas@da-cons.de>
2017-05-29 14:47:56 +0200

    Update and rename test_primes.py to test_addition.py

---
+++ test_addition.py
@@ -0,0 +1,14 @@
+#unit test
+""" doc string"""
+import unittest
+from addition import add
+
+class AdditionTestCase(unittest.TestCase):
+    """Tests for `addtion.py`"""
+
+    def test_is_valid_input(self):
+        """Is five successfully determined to be prime?"""
+        self.assertTrue(add(2, 4))
+
+if __name__ == '__main__':
+    unittest.main()
