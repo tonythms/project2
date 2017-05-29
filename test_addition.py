8701468346953133f41dde52d86f27731ccf96fb
tonythms <tony.thomas@da-cons.de>
2017-05-29 16:17:07 +0200

    Update test_addition.py

--- test_addition.py
+++ test_addition.py
@@ -9,15 +9,15 @@ class AdditionTestCase(unittest.TestCase):
     def test_is_valid_input(self):
         """test?"""
         self.assertTrue(add(4, 5))
-    def test_is_number(self):
-        """test?"""
-        self.assertTrue(A.isdigit())
     def test_is_alphabet(self):
         """test?"""
         self.assertFalse('A'.islower())
     def test_is_valid_input2(self):
         """test?"""
         self.assertTrue(add(4, 5))
-
+    def test_negative_number(self):
+    """Is a negative number correctly determined not to be prime?"""
+    for index in range(-1, -10, -1):
+        self.assertFalse(add(index,index))
 if __name__ == '__main__':
     unittest.main()
