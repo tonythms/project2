bb3efc283d9b9b4bb71d77e7576ce4bb09d5813a
tonythms <tony.thomas@da-cons.de>
2017-05-29 12:53:26 +0200

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,10 +1,10 @@
 # define functions
 """its fun """
 def add(x, y):
-   if int(x) and int(y):
-    return x + y
-   else:
-       return False
+    if int(x) and int(y):
+         return x + y
+      else:
+         return False
 
 # take input from the user
 print "Addition of two numbers"
@@ -12,4 +12,4 @@ print "Addition of two numbers"
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
-print num1,"+",num2,"=", add(num1,num2)
+print num1, "+", num2, "=", add(num1, num2)
