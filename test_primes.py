5f8aee3eafb7d2342d85269bdaf4c7f6fa40d531
tonythms <tony.thomas@da-cons.de>
2017-05-29 14:43:56 +0200

    Create test_primes.py

---
+++ test_primes.py
@@ -0,0 +1,15 @@
+# addition
+""" doc string"""
+def add(X, Y):
+    if int(X) and int(Y):
+        return X+Y
+    else:
+        return False
+
+# take input from the user
+print "Addition of two numbers"
+
+A = raw_input("Enter first number: ")
+B = raw_input("Enter second number: ")
+
+print A, "+", B, "=", add(A, B)
