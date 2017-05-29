980b57192660d0f2cd09ffe72dc40b4c329b2584
tonythms <tony.thomas@da-cons.de>
2017-05-29 15:29:16 +0200

    Update test_addition.py

--- test_addition.py
+++ test_addition.py
@@ -11,7 +11,7 @@ class AdditionTestCase(unittest.TestCase):
         self.assertTrue(add(4, 5))
     def test_is_number(self):
         """test?"""
-        self.assertTrue(isdigit(3, 4))
+        self.assertTrue(A.isdigit())
 
 if __name__ == '__main__':
     unittest.main()
