d0518e8ac6e3712ac34ebe54af2a430b4ae7314d
tonythms <tony.thomas@da-cons.de>
2017-06-12 15:13:37 +0200

    Create test_addition.py

--- test_addition.py
+++ test_addition.py
@@ -9,11 +9,14 @@ class AdditionTestCase(unittest.TestCase):
     def test_is_valid_input(self):
         """checking for input in codes"""
         self.assertTrue(add('4', '5'))
-    def test_is_alphabet(self):
+    def test_is_alphabetl(self):
         """testing for alphabets"""
         self.assertFalse('A'.islower())
     def test_is_valid_input2(self):
         """testing for normal integers"""
         self.assertTrue(add(4, 5))
+    def test_is_alphabetu(self):
+        """testing for alphabets"""
+        self.assertFalse('A'.isupper())
 if __name__ == '__main__':
     unittest.main()
