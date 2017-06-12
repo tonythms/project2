e41148daa8ed654653be570c11ff22685c896471
tonythms <tony.thomas@da-cons.de>
2017-06-12 16:40:03 +0200

    Update test_addition.py

--- test_addition.py
+++ test_addition.py
@@ -1,22 +1,28 @@
 #unit test
 """ doc string"""
 import unittest
-from project2.addition import add
-
+from addition import add
+A='7'
+B='5'
+C='0'
+D='0'
 class AdditionTestCase(unittest.TestCase):
     """Tests for `addtion.py`"""
 
     def test_is_valid_input(self):
         """checking for input in codes"""
-        self.assertTrue(add('4', '5'))
+        self.assertTrue(add(A, B))
+    def test_is_valid_input(self):
+        """checking for input in codes"""
+        self.assertTrue(add(A, C))  
     def test_is_alphabetl(self):
         """testing for alphabets"""
-        self.assertFalse('A'.islower())
+        self.assertFalse(A.islower())
     def test_is_valid_input2(self):
         """testing for normal integers"""
-        self.assertTrue(add('0', '0'))
+        self.assertFalse(add(C, D))
     def test_is_alphabetu(self):
-        """testing for alphabets"""
-        self.assertTrue('A'.isdigit())
+        """testing for digits"""
+        self.assertTrue(A.isdigit())
 if __name__ == '__main__':
     unittest.main()
