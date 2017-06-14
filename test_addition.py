ade22c9c32aa7045ed60b69f07e0b02091c83613
tonythms <tony.thomas@da-cons.de>
2017-06-14 14:32:09 +0200

    Create test_addition.py

--- test_addition.py
+++ test_addition.py
@@ -25,7 +25,7 @@ class AdditionTestCase(unittest.TestCase):
         self.assertTrue(A.isdigit())
     def test_is_true(self):
         """testing for the sum"""
-        if(add(A, B) == '12'):
+        if add(A, B) == '12':
             self.assertTrue(add(A, B))
 if __name__ == '__main__':
     unittest.main()
