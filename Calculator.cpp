a32cfdec0726c0bac4cc08a676b44da2bdc8c205
tonythms <tony.thomas@da-cons.de>
2017-06-14 11:34:01 +0200

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,22 @@
+#include "Calculator.hpp"
+
+int Calculator::Summation(int x, int y)
+{
+   return (x + y);
+}
+
+int Calculator::Difference(int x, int y)
+{
+   return (x - y);
+}
+
+int Calculator::Multiplication(int x, int y)
+{
+   return (x * y);
+}
+
+int Calculator::Division(int x, int y)
+{
+   return (x / y);
+}
+
---
+++ Calculator.hpp
@@ -0,0 +1,14 @@
+#ifndef CALCULATOR_HPP__
+#define CALCULATOR_HPP__
+
+class Calculator
+{
+public:
+   int Summation(int x, int y);
+   int Difference(int x, int y);
+   int Multiplication(int x, int y);
+   int Division(int x, int y);
+};
+
+#endif
+
